0d8c8b926033b29fb643bd3417b5009063c3e42b
rubenjeronimo <ruben.jeronimo.fernandez@gmail.com>
2014-07-11 17:17:56 +0200

    Initial Commit

---
+++ BubbleTranslator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BubbleTranslator
+//
+//  Created by Ruben Jeronimo Fernandez on 11/07/14.
+//  Copyright (c) 2014 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
